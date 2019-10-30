a164f81960128ca6c43c6ae8c1dd7f7f2d1d46ef
A1ex <jaroshov@ukr.net>
2019-10-30 09:26:02 +0200

    30102019

--- app/src/main/java/com/algoritm/terminal/ReceptionAdapter.java
+++ app/src/main/java/com/algoritm/terminal/ReceptionAdapter.java
@@ -5,16 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
 public class ReceptionAdapter extends ArrayAdapter<Reception> {
 
     private ArrayList<Reception> mReceptions;
+    private ArrayList<Reception> mReceptionsTemp;
     private int mResource;
     private LayoutInflater mInflater;
 
@@ -22,6 +21,7 @@ public class ReceptionAdapter extends ArrayAdapter<Reception> {
         super(context, resource, objects);
 
         mReceptions = objects;
+        mReceptionsTemp = (ArrayList<Reception>) mReceptions.clone();
         mResource = resource;
         mInflater = LayoutInflater.from(context);
     }
@@ -38,5 +38,46 @@ public class ReceptionAdapter extends ArrayAdapter<Reception> {
 //        ((TextView) convertView.findViewById(R.id.itemDriverPhone)).setText(reception.getDriverPhone());
 
         return convertView;
+    }
+
+    @Override
+    public Filter getFilter() {
+        Filter filter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults();
+                String seq = constraint.toString().toLowerCase();
+                ArrayList<Reception> list = new ArrayList<>();
+                if (seq.trim().length() == 0 || seq == null) {
+                    results.count = mReceptionsTemp.size();
+                    results.values = mReceptionsTemp;
+                    list.addAll(mReceptionsTemp);
+                } else {
+                    for (Reception reception : mReceptionsTemp) {
+                        if (reception.getDescription().toLowerCase().contains(seq.toLowerCase())
+                                | reception.getAutoNumber().toLowerCase().contains(seq.toLowerCase())
+                                | reception.getDriver().toLowerCase().contains(seq.toLowerCase()))
+
+                            list.add(reception);
+                    }
+                }
+                results.count = list.size();
+                results.values = list;
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                if (results.count > 0) {
+                    mReceptions.clear();
+                    mReceptions.addAll((ArrayList<Reception>) results.values);
+                    notifyDataSetChanged();
+                } else {
+                    notifyDataSetInvalidated();
+                }
+            }
+        };
+
+        return filter;
     }
 }
